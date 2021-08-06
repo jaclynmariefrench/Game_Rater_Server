73c3431c7673c15966703fb859463492f1cd5f06
Jaclyn French <jaclynmariefrench@gmail.com>
2021-08-06 11:38:21 -0500

    forgot to do auth for the server side

--- gamerraterapi/admin.py
+++ gamerraterapi/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from gamerraterapi.models import Categories, GameCategories, GamePics, GameRatings, GameReviews, Games, Players
 
 # Register your models here.
+admin.site.register(Categories)
+admin.site.register(GameCategories)
+admin.site.register(GamePics)
+admin.site.register(GameRatings)
+admin.site.register(GameReviews)
+admin.site.register(Games)
+admin.site.register(Players)
